42caec42cd72350cf4a1c92952bc3cfe238e7e74
Henrique Borges <henrique17_votu@hotmail.com>
2020-09-22 22:10:37 -0300

    Criação das rotas para navegação

--- src/App.js
+++ src/App.js
@@ -4,13 +4,20 @@ import './App.css';
 import BarraNavegacao from './Componentes/BarraNavegacao';
 import Footer from './Componentes/Footer';
 import Home from './Componentes/Home';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import Produtos from './Componentes/Produtos';
 
 function App() {
   return (
     <div className="App">
-      <BarraNavegacao/>
-      <Home/>
-      <Footer/>
+      <Router>
+        <BarraNavegacao/>
+        <Switch>
+          <Route exact path="/" component={Home}></Route>
+          <Route exact path="/produtos" component={Produtos}></Route>
+        </Switch>
+        <Footer/>
+      </Router>
     </div>
   );
 }
---
+++ src/Componentes/Produtos.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Container } from 'react-bootstrap';
+
+class Produtos extends Component {
+    render() {
+        return (
+            <Container>
+                <h1>Produtos</h1>
+            </Container>
+        );
+    }
+}
+
+export default Produtos;
